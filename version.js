c56a66fbb459367bb6683456d8b16ef05e7ece45
mattzmud <matt.zmud@gmail.com>
2026-07-31 16:36:16 -0400

    Add files via upload

---
+++ version.js
@@ -0,0 +1,5 @@
+// version.js
+// Bump this number any time you update firebase-config.js (or other shared
+// files later) and want to force browsers to fetch the fresh copy instead
+// of a cached one. Same pattern as Common Print Co's shared/version.js.
+export const VERSION = '1.0.0';

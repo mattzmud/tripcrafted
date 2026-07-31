e4cc53047e172d6cba0e355de9f74f3a5da6c00d
mattzmud <matt.zmud@gmail.com>
2026-07-31 16:57:42 -0400

    Update version.js

--- version.js
+++ version.js
@@ -2,4 +2,4 @@
 // Bump this number any time you update firebase-config.js (or other shared
 // files later) and want to force browsers to fetch the fresh copy instead
 // of a cached one. Same pattern as Common Print Co's shared/version.js.
-export const VERSION = '1.0.2';
+export const VERSION = '1.0.3';
